beb8d40132f3d0391f6518ebc523f0818d0fd590
tottisan <49814290+MDSZ1@users.noreply.github.com>
2021-02-08 12:45:12 -0300

    Trending movies component

---
+++ src/components/TrendingMovies.vue
@@ -0,0 +1,78 @@
+<template>
+  <section class="trending_movies">
+    <div class="trending_title">
+      <h5>Trending Movies</h5>
+    </div>
+
+    <div class="poster_container">
+      <Movies
+        v-for="movie in trending"
+        :key="movie.id"
+        :poster="movie.poster_path"
+        :id="movie.id"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import Movies from "@/components/Movies";
+
+export default {
+  name: "TrendingMovies",
+
+  components: {
+    Movies
+  },
+
+  /* Vuex test */
+  computed: {
+    ...mapState(["trending"])
+  },
+
+  created() {
+    this.$store.dispatch("trendingSection");
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.trending_movies {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow-x: auto;
+  overflow-y: hidden;
+  height: 19.375em;
+  margin: 0 0 4em;
+
+  .trending_title {
+    align-self: flex-start;
+    padding: 0 1em;
+    margin: 0 0 1em;
+
+    h5 {
+      font-family: "Montserrat", sans-serif;
+    }
+  }
+
+  .poster_container {
+    display: flex;
+    gap: 0.3em;
+    margin: 0 auto;
+    padding: 0 1em;
+  }
+}
+
+@media (min-width: 320px) {
+  .trending_movies {
+    .trending_title {
+      h5 {
+        font-size: 1.4rem;
+      }
+    }
+  }
+}
+</style>
