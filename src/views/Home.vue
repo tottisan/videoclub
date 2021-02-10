4271fc1eec266785f9e584208de5d4fa23f5346d
tottisan <49814290+MDSZ1@users.noreply.github.com>
2021-02-09 22:30:33 -0300

    Home route view

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <HeroMovie />
+    <TrendingMovies />
+    <TrendingSeries />
+  </main>
+</template>
+
+<script>
+import HeroMovie from "@/components/HeroMovie";
+import TrendingMovies from "@/components/TrendingMovies";
+import TrendingSeries from "@/components/TrendingSeries";
+
+export default {
+  name: "Home",
+
+  components: {
+    HeroMovie,
+    TrendingMovies,
+    TrendingSeries
+  }
+};
+</script>
