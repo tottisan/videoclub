c29cb1882d6eddcd6de258eb41b51ef9a45ec259
tottisan <49814290+MDSZ1@users.noreply.github.com>
2021-02-16 21:45:04 -0300

    Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,62 @@
+<template>
+  <footer>
+
+    <div class="column">
+      <h1>Videoclub</h1>
+    </div>
+
+    <div class="column">
+      <a href="https://github.com/tottisan"
+        ><img
+          src="https://icongr.am/fontawesome/github.svg?size=36&color=000000"
+          alt="Github logo"
+      /></a>
+    </div>
+
+    <div class="column">
+      <a href="https://www.themoviedb.org"
+        ><img
+          src="@/assets/tmdb.svg"
+          alt="The Movie Database Logo"
+          class="tmdb-logo"
+      /></a>
+    </div>
+
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 4.25em;
+  padding: 0 1em;
+
+  .column {
+    h1 {
+      font-size: 1.3rem;
+      font-family: "Montserrat", sans-serif;
+      font-weight: 700;
+      color: #2c3f74;
+      text-transform: uppercase;
+      text-align: center;
+      margin: 0;
+    }
+
+    .tmdb-logo {
+      width: 3em;
+    }
+
+    a {
+      text-decoration: none;
+    }
+  }
+}
+</style>
